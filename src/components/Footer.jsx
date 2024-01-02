63ac6ca0ef25e5f041c0117c33e4765bcbf30200
VivekZ79 <viveks03realm@gmail.com>
2024-01-03 03:21:37 +0530

    Changes made

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className=" bg-gradient-to-r from-[#FFDCAB] to-[#AB6B2E] text-black rounded-t-3xl mt-8 md:mt-0">
       <div className="flex flex-col md:flex-row justify-between p-8 md:px-32 px-5">
         <div className=" w-full md:w-1/4">
-          <h1 className=" font-semibold text-xl pb-4"><Creve-Coffee></Creve-Coffee></h1>
+          <h1 className=" font-semibold text-xl pb-4">Creve-Coffee</h1>
           <p className=" text-sm">
             Welcome to our coffee haven! Explore our aromatic brews, savor
             artisanal flavors, and discover the perfect roast to elevate your
